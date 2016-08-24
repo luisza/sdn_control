a69a2e618f2c695570bbbf6710c3df37bb1f5e92
Luis Zárate <luis.zarate@solvosoft.com>
2016-08-24 15:58:49 -0600

    Fixed installer

--- ryu/installer.sh
+++ ryu/installer.sh
@@ -3,14 +3,16 @@
 # ./installer.sh apps/simple_switch_13.py
 
 mkdir -p /etc/ryuctrl/
+rm -rf /etc/ryuctrl/apps
 cp -a apps /etc/ryuctrl/
 
+rm /etc/systemd/system/ryu.service
 cp ryu.service.default /etc/systemd/system/ryu.service
 chmod 664 /etc/systemd/system/ryu.service
 
 ryu_service=$1
 
-sed -i "s/APPS/\/etc\/ryuctrl\/"$ryu_service"/g" /etc/systemd/system/ryu.service
+sed -i "s/APPS/\/etc\/ryuctrl\/apps/ "$ryu_service"/g" /etc/systemd/system/ryu.service
 
 systemctl daemon-reload
 systemctl enable ryu.service
